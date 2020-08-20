4acda08a07ce0dfffadd35f2029f53c99e48debf
Maksym Hlukhov <holosievo@gmail.com>
2020-08-20 22:08:13 +0300

    refactoring to common crud interface. closes #12

---
+++ pet-clinic-data/src/main/java/io/mh/sfgpetclinic/services/GenericCrudService.java
@@ -0,0 +1,16 @@
+package io.mh.sfgpetclinic.services;
+
+import java.util.Set;
+
+public interface GenericCrudService<T, ID> {
+
+	Set<T> findAll();
+
+	T findById(ID id);
+
+	T save(T object);
+
+	void deleteById(ID id);
+
+	void delete(T object);
+}
--- pet-clinic-data/src/main/java/io/mh/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/io/mh/sfgpetclinic/services/PetService.java
@@ -1,14 +1,6 @@
 package io.mh.sfgpetclinic.services;
 
 import io.mh.sfgpetclinic.model.Pet;
-import java.util.Set;
 
-public interface PetService {
-
-	Pet findById(Long id);
-
-	Pet save(Pet pet);
-
-	Set<Pet> findAll();
-
+public interface PetService extends GenericCrudService<Pet, Long> {
 }
