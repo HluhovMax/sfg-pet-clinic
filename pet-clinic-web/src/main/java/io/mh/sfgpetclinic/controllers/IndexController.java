967922500f760bf2f13281c651d53e401840e28c
Maksym Hlukhov <holosievo@gmail.com>
2020-08-21 00:08:03 +0300

    adding vets page and controller. closes #7

--- pet-clinic-web/src/main/java/io/mh/sfgpetclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/io/mh/sfgpetclinic/controllers/IndexController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
-	@GetMapping({"", "/", "index", "index.html"})
+	@GetMapping({"", "/", "/index", "/index.html"})
 	public String index(Model model) {
 		model.addAttribute("text", "Index Page");
 		return "index";
