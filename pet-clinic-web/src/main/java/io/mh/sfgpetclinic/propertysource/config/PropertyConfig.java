2a4610f5ff33debbfc298636b9a38134835e3dc6
holosievo@gmail.com <holosievo@gmail.com>
2020-09-17 00:28:22 +0300

    yml approach

--- pet-clinic-web/src/main/java/io/mh/sfgpetclinic/propertysource/config/PropertyConfig.java
+++ pet-clinic-web/src/main/java/io/mh/sfgpetclinic/propertysource/config/PropertyConfig.java
@@ -1,26 +1,13 @@
 package io.mh.sfgpetclinic.propertysource.config;
 
-import io.mh.sfgpetclinic.propertysource.factory.YamlPropertySourceFactory;
 import io.mh.sfgpetclinic.propertysource.model.FakeDataSource;
 import io.mh.sfgpetclinic.propertysource.model.FakeJmsBroker;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
-@PropertySources({
-	@PropertySource(value = {"classpath:jms.yml"}, factory = YamlPropertySourceFactory.class)
-})
 public class PropertyConfig {
-
-//	final Environment environment;
-//
-//	public PropertyConfig(Environment environment) {
-//		this.environment = environment;
-//	}
 
 	// .PROPERTIES
 	@Value("${mh.username}")
@@ -51,15 +38,9 @@ public class PropertyConfig {
 	public FakeDataSource fakeDataSource() {
 		return new FakeDataSource
 			.Builder()
-//			.user(environment.getProperty("USERNAME"))
 			.user(user)
 			.password(password)
 			.url(url)
 			.build();
-	}
-
-	@Bean
-	public static PropertySourcesPlaceholderConfigurer properties() {
-		return new PropertySourcesPlaceholderConfigurer();
 	}
 }
