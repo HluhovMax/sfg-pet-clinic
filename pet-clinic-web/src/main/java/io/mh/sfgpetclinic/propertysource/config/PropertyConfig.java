a61c52d08baf1b4553433c2bf8719e27d16bd4b9
holosievo@gmail.com <holosievo@gmail.com>
2020-09-16 22:54:01 +0300

    added Environment property config example

--- pet-clinic-web/src/main/java/io/mh/sfgpetclinic/propertysource/config/PropertyConfig.java
+++ pet-clinic-web/src/main/java/io/mh/sfgpetclinic/propertysource/config/PropertyConfig.java
@@ -1,15 +1,20 @@
 package io.mh.sfgpetclinic.propertysource.config;
 
 import io.mh.sfgpetclinic.propertysource.model.FakeDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
+
+	@Autowired
+	Environment environment;
 
 	@Value("${mh.username}")
 	String user;
@@ -22,7 +27,7 @@ public class PropertyConfig {
 	public FakeDataSource fakeDataSource() {
 		return new FakeDataSource
 			.Builder()
-			.user(user)
+			.user(environment.getProperty("USERNAME"))
 			.password(password)
 			.url(url)
 			.build();
