969d75ee5fa6ad095fe73db2a05f6d6e2e6bc023
holosievo@gmail.com <holosievo@gmail.com>
2020-09-18 18:29:09 +0300

    optimized imports

--- pet-clinic-web/src/main/java/io/mh/sfgpetclinic/SfgPetClinicApplication.java
+++ pet-clinic-web/src/main/java/io/mh/sfgpetclinic/SfgPetClinicApplication.java
@@ -1,10 +1,7 @@
 package io.mh.sfgpetclinic;
 
-import io.mh.sfgpetclinic.propertysource.model.FakeDataSource;
-import io.mh.sfgpetclinic.propertysource.model.FakeJmsBroker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SfgPetClinicApplication {
